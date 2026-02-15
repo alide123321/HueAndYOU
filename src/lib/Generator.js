a00d6b0e1aae427e004450f30d71e65188ec833d
DeAndre Josey <dtj5156@psu.edu>
2026-02-15 03:33:35 -0500

    CAP-25: cleaned and fixed Generator strategy wiring

--- src/lib/Generator.js
+++ src/lib/Generator.js
@@ -1,73 +1,69 @@
 import {GenerationSettings} from '../../shared/types/GenerationSettings.js';
 import {ColorHarmony} from '../../shared/utils/constants.js';
-import {Palette} from '../../shared/types/Palette.js';
-import {Complementary} from '../harmony/ComplementaryHSL.js';
+import {Complementary as ComplementaryOKLCH} from '../harmony/ComplementaryOKLCH.js';
+import {Complementary as ComplementaryHSL} from '../harmony/ComplementaryHSL.js';
 import {Monochromatic} from '../harmony/Monochromatic.js';
+import {Triadic} from '../harmony/Triadic.js';
+import {Analogous} from '../harmony/Analogous.js';
 import {Tetradic} from '../harmony/Tetradic.js';
-
 
 /**
  * Generator class
  * @author Ian Timchak
  */
 export class Generator {
-  // controller for turning requests into palette generation
   constructor() {
     this.generationSettings = null;
     this.selectedStrategy = null;
   }
 
-
-  /**
-   * @author Ian Timchak
-   * @param {GenerationSettings} settings
-   */
   applySettings(settings) {
     this.generationSettings = settings;
 
-    // build strategy based on settings
-    //Todo: change the signature of GenerationSettings to pass in strategy objects directly.
-    // e.g., settings.strategy = new ComplimentaryHSL();
-    // Then, we can do polymorphic calls without the switch statement.
-    // For prototype, this will be sufficient.
     switch (settings.harmonyType) {
       case ColorHarmony.COMPLEMENTARY:
-        this.selectedStrategy = new Complementary();
+        // Switch between OKLCH and HSL if desired
+        this.selectedStrategy = new ComplementaryOKLCH();
+        // this.selectedStrategy = new ComplementaryHSL();
         break;
-      
-        case ColorHarmony.MONOCHROMATIC:
-  // Monochromatic harmony strategy
-  // Added by DeAndre Josey (CAP-23)
-  console.log('CAP-23: Monochromatic strategy selected');
-  this.selectedStrategy = new Monochromatic();
-  break;
 
-  case ColorHarmony.TETRADIC:
-  // Tetradic harmony strategy
-  // Added by DeAndre Josey (CAP-25)
-  console.log('CAP-25: Tetradic strategy selected');
-  this.selectedStrategy = new Tetradic();
-  break;
+      case ColorHarmony.MONOCHROMATIC:
+        console.log('CAP-23: Monochromatic strategy selected');
+        this.selectedStrategy = new Monochromatic();
+        break;
 
+      case ColorHarmony.TRIADIC:
+        console.log('CAP-24: Triadic strategy selected');
+        this.selectedStrategy = new Triadic();
+        break;
+
+      case ColorHarmony.ANALOGOUS:
+        this.selectedStrategy = new Analogous();
+        break;
+
+      case ColorHarmony.TETRADIC:
+        console.log('CAP-25: Tetradic strategy selected');
+        this.selectedStrategy = new Tetradic();
+        break;
 
       default:
         throw new Error(`Unsupported harmony type: ${settings.harmonyType}`);
     }
   }
-  
+
   generate() {
     if (!this.selectedStrategy) {
-      throw new Error('No generation strategy selected. Please apply settings first.');
+      throw new Error(
+        'No generation strategy selected. Please apply settings first.'
+      );
     }
 
     const numberOfPalettes = this.generationSettings.numberOfPalettes || 1;
     const baseSeed = this.generationSettings.seed;
 
     let palettes = [];
-    
 
     for (let i = 0; i < numberOfPalettes; i++) {
-      // update seed predictably for each palette to ensure reproducibility
       this.generationSettings.seed = baseSeed + i;
       const palette = this.selectedStrategy.buildPalette(this.generationSettings);
       palettes.push(palette);
