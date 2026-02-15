c38e729d2189fb5a4640f4604daf79e16dc7bbdc
DeAndre Josey <dtj5156@psu.edu>
2026-02-15 03:56:59 -0500

    CAP-25: restore Generator annotations and remove duplicate palette line

--- src/lib/Generator.js
+++ src/lib/Generator.js
@@ -12,36 +12,54 @@ import {Tetradic} from '../harmony/Tetradic.js';
  * @author Ian Timchak
  */
 export class Generator {
+  // controller for turning requests into palette generation
   constructor() {
     this.generationSettings = null;
     this.selectedStrategy = null;
   }
 
+  /**
+   * @author Ian Timchak, Ali Aldaghishy
+   * @param {GenerationSettings} settings
+   */
   applySettings(settings) {
     this.generationSettings = settings;
 
+    // build strategy based on settings
+    //Todo: change the signature of GenerationSettings to pass in strategy objects directly.
+    // e.g., settings.strategy = new ComplimentaryHSL();
+    // Then, we can do polymorphic calls without the switch statement.
+    // For prototype, this will be sufficient.
     switch (settings.harmonyType) {
       case ColorHarmony.COMPLEMENTARY:
-        // Switch between OKLCH and HSL if desired
+        // You can switch between ComplementaryHSL and ComplementaryOKLCH here
         this.selectedStrategy = new ComplementaryOKLCH();
         // this.selectedStrategy = new ComplementaryHSL();
         break;
 
       case ColorHarmony.MONOCHROMATIC:
+        // Monochromatic harmony strategy
+        // Added by DeAndre Josey (CAP-23)
         console.log('CAP-23: Monochromatic strategy selected');
         this.selectedStrategy = new Monochromatic();
         break;
 
       case ColorHarmony.TRIADIC:
+        // Triadic harmony strategy
+        // Added by DeAndre Josey (CAP-24)
         console.log('CAP-24: Triadic strategy selected');
         this.selectedStrategy = new Triadic();
         break;
 
       case ColorHarmony.ANALOGOUS:
+        // Analogous harmony strategy
+        // Added by Ian Timchak (CAP-22)
         this.selectedStrategy = new Analogous();
         break;
 
       case ColorHarmony.TETRADIC:
+        // Tetradic harmony strategy
+        // Added by DeAndre Josey (CAP-25)
         console.log('CAP-25: Tetradic strategy selected');
         this.selectedStrategy = new Tetradic();
         break;
@@ -64,6 +82,7 @@ export class Generator {
     let palettes = [];
 
     for (let i = 0; i < numberOfPalettes; i++) {
+      // update seed predictably for each palette to ensure reproducibility
       this.generationSettings.seed = baseSeed + i;
       const palette = this.selectedStrategy.buildPalette(this.generationSettings);
       palettes.push(palette);
